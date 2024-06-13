51797c73546c5d72837d1f180cd7b075215292fe
Farouk Allani <81933547+farouk-allani@users.noreply.github.com>
2024-06-14 00:48:09 +0100

    feat: init dashboard page and reward component

---
+++ components/dashboard/RewardSystem.tsx
@@ -0,0 +1,73 @@
+import * as React from "react";
+
+type RewardOptionProps = {
+  title: string;
+  description: string;
+};
+
+const RewardOption: React.FC<RewardOptionProps> = ({ title, description }) => (
+  <section className="flex flex-col grow p-6 mx-auto w-full font-semibold bg-white rounded-3xl border border-solid border-zinc-300 max-md:px-5 max-md:mt-8">
+    <h2 className="flex gap-3.5 text-2xl text-black">
+      {title}
+      <a
+        href="#en-savoir-plus"
+        className="self-start pb-1.5 mt-1 text-base text-black border-b-2 border-black border-solid"
+      >
+        En savoir plus
+      </a>
+    </h2>
+    <p className="mt-8 text-lg font-medium text-black">{description}</p>
+    <a
+      href="#en-savoir-plus"
+      className="justify-center self-start px-6 py-3 mt-8 text-base text-black bg-emerald-200 rounded-lg border-2 border-black border-solid max-md:px-5"
+    >
+      En savoir plus
+    </a>
+  </section>
+);
+
+const RewardSystem: React.FC = () => {
+  const rewardOptions = [
+    {
+      title: "Staking",
+      description:
+        "Holding $ERA tokens offers a stake in erable's growth, with their value poised to increase through strategic buybacks and burns as the ecosystem expands.",
+    },
+    {
+      title: "Airdrop",
+      description:
+        "Holding $ERA tokens offers a stake in erable's growth, with their value poised to increase through strategic buybacks and burns as the ecosystem expands.",
+    },
+    {
+      title: "LP Farming",
+      description:
+        "Holding $ERA tokens offers a stake in erable's growth, with their value poised to increase through strategic buybacks and burns as the ecosystem expands.",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col items-start self-stretch p-20 bg-white max-md:px-5">
+      <header>
+        <h1 className="mt-2.5 ml-2.5 text-5xl font-semibold text-black max-md:max-w-full max-md:text-4xl">
+          Discover Our Reward Systems
+        </h1>
+        <h2 className="mt-2.5 ml-2.5 text-lg font-medium text-black max-md:max-w-full">
+          Choose one option and get rewards
+        </h2>
+      </header>
+      <main className="justify-between self-stretch mx-2.5 mt-10 max-md:max-w-full">
+        <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+          {rewardOptions.map((option) => (
+            <RewardOption
+              key={option.title}
+              title={option.title}
+              description={option.description}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default RewardSystem;
