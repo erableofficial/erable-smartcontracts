567a1ce58c18b9fc3364bc9aea4406dda0a88d31
Ayoub Amer <ayoubamerrr290@gmail.com>
2024-06-14 15:44:03 +0100

    initialize contract after deployment

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -13,6 +13,22 @@ async function main() {
   const staking = await Staking.deploy();
 
   console.log("Staking contract deployed to: ", staking.target);
+
+  // call intialize function on staking contract
+  await staking.initialize(
+    myToken.target,
+    365 * 24 * 60 * 60, // stakingDuration: 1 year (in seconds)
+    50, // yieldConstant: A starting yield
+    30 * 24 * 60 * 60, // cooldownPeriod: 30 days (in seconds)
+    5, // startingSlashingPoint: 5% penalty
+    2 // monthlyIncreasePercentage: 2% penalty increase
+  );
+  console.log("Staking contract initialized");
+
+  // Approve Staking contract to spend tokens
+  await myToken.approve(staking.target, ethers.parseEther("1000"));
+
+  console.log("Approved staking contract to spend tokens");
 }
 
 main()
