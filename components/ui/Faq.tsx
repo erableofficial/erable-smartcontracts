79419db5693b2dd9c39a6400702b9de45399a3c2
Farouk Allani <81933547+farouk-allani@users.noreply.github.com>
2024-06-18 11:30:31 +0100

    feat: make accordion expandable in a smooth way

--- components/ui/Faq.tsx
+++ components/ui/Faq.tsx
@@ -1,42 +1,88 @@
-import { Plus } from "lucide-react";
+import { Plus, Minus } from "lucide-react";
 import Link from "next/link";
 import * as React from "react";
 
 type FAQItemProps = {
   question: string;
-  iconSrc: string;
+  answer: string;
+  link?: string;
 };
 
-const FAQItem: React.FC<FAQItemProps> = ({ question, iconSrc }) => (
-  <div className="flex flex-col justify-center px-6 py-4 mt-6 bg-white rounded-2xl border border-solid border-stone-300 max-md:px-5 max-md:max-w-full">
-    <div className="flex gap-5 justify-between">
-      <div className="my-auto text-xl font-bold text-primary max-md:max-w-full">
-        {question}
+const FAQItem: React.FC<FAQItemProps> = ({ question, answer, link }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const contentRef = React.useRef<HTMLDivElement>(null);
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // This useEffect ensures that updates to isOpen appropriately adjust maxHeight
+  React.useEffect(() => {
+    if (contentRef.current) {
+      contentRef.current.style.maxHeight = isOpen
+        ? `${contentRef.current.scrollHeight}px`
+        : "0";
+    }
+  }, [isOpen]);
+
+  return (
+    <div className="flex flex-col justify-center px-6 py-4 mt-6 bg-white rounded-2xl border border-solid border-stone-300 max-md:px-5 max-md:max-w-full">
+      <div className="flex gap-5 justify-between">
+        <div className="my-auto text-xl font-bold text-primary max-md:max-w-full">
+          {question}
+        </div>
+        <button
+          className="flex justify-center items-center p-3 bg-surface-500 border border-solid border-zinc-300 h-[2.813rem] rounded-[2.344rem] w-[2.813rem]"
+          onClick={toggleOpen}
+        >
+          {isOpen ? <Minus /> : <Plus />}
+        </button>
       </div>
-      <button className="flex justify-center items-center p-3 bg-surface-500 border border-solid border-zinc-300 h-[2.813rem] rounded-[2.344rem] w-[2.813rem]">
-        <Plus />
-      </button>
+      <div
+        ref={contentRef}
+        className="overflow-hidden transition-max-height duration-500 ease-in-out"
+        style={{ maxHeight: "0" }}
+      >
+        <div className="mt-4 text-sm text-gray-700">{answer}</div>
+        {link && (
+          <div className="self-start pb-1.5 mt-6 text-lg font-semibold text-black ">
+            <Link
+              href={link}
+              className="pb-1.5 mt-6 text-lg font-semibold text-black border-b-2 border-black"
+            >
+              Read more
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const Faq: React.FC = () => {
   const faqItems = [
     {
       question: "Lorem ipsum dolor sit amet consectetu or sit?",
-      iconSrc: "/images/plus.svg",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      link: "#",
     },
     {
       question: "Lorem ipsum dolor sit amet consectetu or sit?",
-      iconSrc: "/images/plus.svg",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      link: "#",
     },
     {
       question: "Lorem ipsum dolor sit amet consectetu or sit?",
-      iconSrc: "/images/plus.svg",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      link: "#",
     },
     {
       question: "Lorem ipsum dolor sit amet consectetu or sit?",
-      iconSrc: "/images/plus.svg",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      link: "#",
     },
   ];
 
@@ -73,7 +119,8 @@ const Faq: React.FC = () => {
               <FAQItem
                 key={index}
                 question={item.question}
-                iconSrc={item.iconSrc}
+                answer={item.answer}
+                link={item.link}
               />
             ))}
           </div>
