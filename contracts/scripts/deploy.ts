c29f34adbcd45ef52684ba5a0ec64e7a9f16e1ea
Ayoub Amer <ayouub.ameur@gmail.com>
2024-06-20 11:16:24 +0100

    update deploying script

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,36 +1,33 @@
-import {ethers, upgrades} from 'hardhat'
+import { ethers, upgrades } from "hardhat";
 
 async function main() {
-
-  console.log(ethers)
+  // console.log(ethers);
   const [deployer] = await ethers.getSigners();
 
-  const MyToken = await ethers.getContractFactory("StakingToken");
-  const myToken = await MyToken.deploy(ethers.parseEther("8000"));
+  const StakingToken = await ethers.getContractFactory("StakingToken");
+  const stakingToken = await StakingToken.deploy(
+    ethers.parseEther("1000000000")
+  );
 
   await stakingToken.waitForDeployment();
   console.log("StakingToken deployed to:", stakingToken.target);
 
   const Staking = await ethers.getContractFactory("Staking");
-  const staking = await Staking.deploy();
-
-  console.log("Staking contract deployed to: ", staking.target);
-
-  // call intialize function on staking contract
-  await staking.initialize(
-    myToken.target,
-    31556926n, // 1 year in seconds
-    800000000000000n, // yield constant
-    60, // cooldown period in seconds
-    600000000000000000n, // starting slashing point
-    480000000000000000n // monthly increase percentage
+  const staking = await upgrades.deployProxy(
+    Staking,
+    [
+      stakingToken.target,
+      31556926n, // 1 year in seconds
+      800000000000000n, // yield constant
+      300n, // cooldown period in seconds
+      600000000000000000n, // starting slashing point
+      480000000000000000n, // monthly increase percentage
+    ],
+    { initializer: "initialize" }
   );
-  console.log("Staking contract initialized");
 
-  // Approve Staking contract to spend tokens
-  await myToken.approve(staking.target, ethers.parseEther("8000"));
-
-  console.log("Approved staking contract to spend tokens");
+  await staking.waitForDeployment();
+  console.log("Staking contract deployed to:", staking.target);
 }
 
 main().catch((error) => {
