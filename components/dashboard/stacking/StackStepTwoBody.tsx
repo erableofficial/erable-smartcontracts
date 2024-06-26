3ab899159b141312e4780249fab3d937aeda64c9
Ayoub Amer <ayouub.ameur@gmail.com>
2024-06-26 16:28:12 +0100

    config step 2 loading model sync

--- components/dashboard/stacking/StackStepTwoBody.tsx
+++ components/dashboard/stacking/StackStepTwoBody.tsx
@@ -5,7 +5,6 @@ import { useWaitForTransactionReceipt, useWriteContract } from "wagmi";
 import { parseEther } from "viem";
 import CustomToast from "../CustomToast";
 import { Check, Info, TriangleAlert } from "lucide-react";
-import SignLoadingModal from "../SignLoadingModal";
 import InfoText from "../../ui/InfoText";
 import { InfoBox } from "../AuthorizeStackingModal";
 import StackingLoadingModal from "../StackingLoadingModal";
@@ -30,6 +29,22 @@ type StackStepTwoBodyProps = {
   amount: number;
 };
 
+type ErrorBoxProps = {
+  text: string;
+  bgColor: string;
+};
+
+const ErrorBox: React.FC<ErrorBoxProps> = ({ text, bgColor }) => (
+  <div
+    className={`flex gap-1.5 px-2.5 py-1 font-medium ${bgColor} rounded font-NeueHaas text-lg leading-[21.6px] `}
+  >
+    <div>
+      <TriangleAlert width={17} height={17} color="#000000" />
+    </div>
+    <div>{text}</div>
+  </div>
+);
+
 const StackStepTwoBody: React.FC<StackStepTwoBodyProps> = ({
   setSteps,
   infoCards,
@@ -37,7 +52,7 @@ const StackStepTwoBody: React.FC<StackStepTwoBodyProps> = ({
 }) => {
   const [toggleStackingLoadingModal, setToggleStackingLoadingModal] =
     React.useState(false);
-  const [showSignModal, setShowSignModal] = React.useState(false);
+
   const items = [
     { label: "Total APR:", value: "00" },
     { label: "Duration:", value: infoCards[1].value },
@@ -53,6 +68,7 @@ const StackStepTwoBody: React.FC<StackStepTwoBodyProps> = ({
 
   React.useEffect(() => {
     if (isConfirmed) {
+      setToggleStackingLoadingModal(false);
       toast.success(
         <CustomToast
           title="Transaction confirmed."
@@ -65,46 +81,36 @@ const StackStepTwoBody: React.FC<StackStepTwoBodyProps> = ({
           icon: <Check width={21} height={21} size={32} color="#21725E" />,
         }
       );
-      // wait for 2.5 second before setting the steps
-      setTimeout(() => {
-        setShowSignModal(false);
-        setSteps([
-          {
-            number: "1",
-            title: "Stake your token : Informations",
-            text: "Staking Informations",
-            isActive: false,
-          },
-          {
-            number: "2",
-            title: "Send funds",
-            text: "Send funds",
-            isActive: false,
-          },
-          {
-            number: "3",
-            title: "You staked sucessfully",
-            text: "Confirmation",
-            isActive: true,
-          },
-        ]);
-      }, 2500);
+      setSteps([
+        {
+          number: "1",
+          title: "Stake your token : Informations",
+          text: "Staking Informations",
+          isActive: false,
+        },
+        {
+          number: "2",
+          title: "Send funds",
+          text: "Send funds",
+          isActive: false,
+        },
+        {
+          number: "3",
+          title: "You staked sucessfully",
+          text: "Confirmation",
+          isActive: true,
+        },
+      ]);
     }
   }, [isConfirmed]);
 
   // error
   React.useEffect(() => {
     if (error) {
+      setToggleStackingLoadingModal(false);
       toast.error(
-        <CustomToast
-          title="Something went wrong"
-          message="When you do something noble and beautiful and nobody noticed, do not be
-        sad. For the sun every morning is a beautiful spectacle and yet most of
-        the audience still sleeps."
-        />,
+        <CustomToast title="Something went wrong" message={error.message} />,
         {
-          // icon: <Info />,
-          // autoClose: 5000000,
           theme: "colored",
           icon: (
             <TriangleAlert width={21} height={21} size={32} color="#B91C1C" />
@@ -133,11 +139,11 @@ const StackStepTwoBody: React.FC<StackStepTwoBodyProps> = ({
           icon: <Info width={21} height={21} size={32} color="#0000" />,
         }
       );
-      setShowSignModal(true);
     }
   }, [hash]);
 
   const handleSendFund = () => {
+    setToggleStackingLoadingModal(true);
     writeContract({
       abi: contractABI,
       address: contractAddress,
@@ -146,63 +152,78 @@ const StackStepTwoBody: React.FC<StackStepTwoBodyProps> = ({
     });
   };
 
-  if (showSignModal) {
-    return <SignLoadingModal />;
-  } else {
-    return (
-      <div>
-        <div className="flex mx-auto flex-col p-10 mt-14 bg-white rounded-3xl border border-solid border-stone-300 max-w-[791px] max-md:px-5">
-          <div className="text-3xl font-semibold text-neutral-700 max-md:max-w-full">
-            Confirm Fund Transfer
-          </div>
-          <p className=" text-lg mt-3">
-            Now, let’s secure your rewards! Please confirm the transfer of your
-            tokens into the staking contract. This action will lock your tokens
-            for one year, starting your earnings. Remember, withdrawing funds
-            before the term ends may result in penalties. This transfer might
-            take a few moments.
-          </p>
-          <div className="mt-10 text-xl font-bold text-neutral-700 max-md:max-w-full">
-            Your staking:
-          </div>
-          <section className="flex gap-5 justify-between mt-2.5 max-md:flex-wrap max-md:max-w-full">
-            <div className="text-3xl font-semibold text-neutral-700">
-              {amount} $ERA
-            </div>
-            <div className="my-auto text-base font-medium text-neutral-500">
-              =$250.000
-            </div>
-          </section>
-          <hr className="shrink-0 mt-6 h-px border border-solid bg-neutral-300 border-neutral-300 max-md:max-w-full" />
-          <div className="mt-6 text-xl font-bold text-neutral-700 max-md:max-w-full">
-            Staking key informations:
-          </div>
-          <section className="flex gap-5 justify-between mt-6 text-lg font-medium text-neutral-700 max-md:flex-wrap max-md:max-w-full">
-            <div className="flex flex-col">
-              {items.map((item, index) => (
-                <div key={index} className={index > 0 ? "mt-5" : ""}>
-                  {item.label}
-                </div>
-              ))}
-            </div>
-            <div className="flex flex-col">
-              {items.map((item, index) => (
-                <div key={index} className={index > 0 ? "mt-5" : ""}>
-                  {item.value}
-                </div>
-              ))}
-            </div>
-          </section>
-          <button
-            className="justify-center self-center px-7 py-4 mt-10 text-lg font-semibold text-neutral-700 bg-emerald-200 rounded-xl border-black border-solid border-[3px] max-md:px-5"
-            onClick={handleClick}
-          >
-            Send funds
-          </button>
+  return (
+    <div>
+      <div className="flex mx-auto flex-col p-10 mt-14 bg-white rounded-3xl border border-solid border-stone-300 max-w-[791px] max-md:px-5">
+        <StackingLoadingModal
+          toggleStackingLoadingModal={toggleStackingLoadingModal}
+        />
+        <div className="text-3xl mb-3 font-semibold text-neutral-700 max-md:max-w-full">
+          Confirm Fund Transfer
         </div>
+
+        <InfoBox
+          text="Now, let’s secure your rewards! Please confirm the transfer of your tokens into the staking contract. This action will lock your tokens for one year, starting your earnings."
+          bgColor="bg-surface-500"
+        />
+
+        <div className="mt-10 text-xl font-bold text-neutral-700 max-md:max-w-full">
+          Your staking:
+        </div>
+        <section className="flex gap-5 justify-between mt-2.5 max-md:flex-wrap max-md:max-w-full">
+          <div className="text-3xl font-semibold text-neutral-700">
+            {amount} $ERA
+          </div>
+          <div className="my-auto text-base font-medium text-neutral-500">
+            =$250.000
+          </div>
+        </section>
+        <hr className="shrink-0 mt-6 h-px border border-solid bg-neutral-300 border-neutral-300 max-md:max-w-full" />
+        <div className="mt-6 text-xl font-bold text-neutral-700 max-md:max-w-full">
+          Staking key informations:
+        </div>
+        <section className="flex gap-5 justify-between mt-6 text-lg font-medium text-neutral-700 max-md:flex-wrap max-md:max-w-full">
+          <div className="flex flex-col">
+            {items.map((item, index) => (
+              <div key={index} className={index > 0 ? "mt-5" : ""}>
+                {item.label}
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-col">
+            {items.map((item, index) => (
+              <div key={index} className={index > 0 ? "mt-5" : ""}>
+                {item.value}
+              </div>
+            ))}
+          </div>
+        </section>
+        <div className=" py-10">
+          <InfoBox
+            text="Remember, withdrawing funds before the term ends may result in penalties. This transfer might take a few moments."
+            bgColor="bg-warning-200"
+          />
+        </div>
+        {error && (
+          <div className="flex justify-center items-center pb-4">
+            <ErrorBox
+              text="Transaction faliled, try again !"
+              bgColor="bg-[#FFD4D4]"
+            />
+          </div>
+        )}
+        <button
+          className="justify-center self-center px-7 py-4 text-lg font-semibold text-neutral-700 bg-emerald-200 rounded-xl border-black border-solid border-[3px] max-md:px-5"
+          onClick={() => {
+            setToggleStackingLoadingModal(true);
+            handleSendFund();
+          }}
+        >
+          Send funds
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default StackStepTwoBody;
