8aa5f28e9275279b5e263011de38b20efed1e703
Farouk Allani <81933547+farouk-allani@users.noreply.github.com>
2024-06-27 08:43:28 +0100

    feat: update step 3 body text

--- components/dashboard/stacking/StackStepThreeBody.tsx
+++ components/dashboard/stacking/StackStepThreeBody.tsx
@@ -6,9 +6,7 @@ type StackStepThreeBodyProps = {
   amount: number;
 };
 
-const StackStepThreeBody: React.FC<StackStepThreeBodyProps> = ({
-  amount
-}) => {
+const StackStepThreeBody: React.FC<StackStepThreeBodyProps> = ({ amount }) => {
   return (
     <div className=" mt-14 mx-auto flex flex-col items-center p-10 bg-white rounded-3xl border border-solid border-stone-300 max-w-[977px] max-md:px-5">
       <div className="flex justify-center items-center px-3 bg-yellow-200 h-[45px] rounded-[37.5px] w-[45px]">
@@ -18,8 +16,9 @@ const StackStepThreeBody: React.FC<StackStepThreeBodyProps> = ({
         {amount} $ERA staked
       </h1>
       <p className="mt-6 text-lg font-medium text-center text-neutral-700 max-w-[572px] max-md:max-w-full">
-        Lorem ipsum dolor sit amet consectetur. Sed consectetur erat feugiat
-        felis pharetra mauris neque id
+        Congratulations! You have successfully staked. Your tokens are now
+        earning rewards, which will accumulate over the staking period. Thank
+        you for participating in our staking program.
       </p>
 
       <div className="flex gap-5 justify-center mt-10 text-lg font-semibold text-neutral-700">
