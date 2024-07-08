f3383172570b67ade04e64a57da0b2738748ec7f
Ayoub Amer <ayouub.ameur@gmail.com>
2024-07-08 14:44:30 +0100

    style: delete all ";" in staking step two

--- components/dashboard/stacking/StackStepTwoBody.tsx
+++ components/dashboard/stacking/StackStepTwoBody.tsx
@@ -39,10 +39,10 @@ const StackStepTwoBody: React.FC<StackStepTwoBodyProps> = ({
     React.useState(false);
 
   const items = [
-    { label: "Total APR:", value: "00" },
-    { label: "Duration:", value: infoCards[1].value },
-    { label: "Start date:", value: infoCards[2].value },
-    { label: "End date:", value: infoCards[3].value },
+    { label: "Total APR", value: "00" },
+    { label: "Duration", value: infoCards[1].value },
+    { label: "Start date", value: infoCards[2].value },
+    { label: "End date", value: infoCards[3].value },
   ];
   const { writeContract, data: hash, error, isPending } = useWriteContract();
 
@@ -157,7 +157,7 @@ const StackStepTwoBody: React.FC<StackStepTwoBodyProps> = ({
         />
 
         <div className="mt-10 text-xl font-bold text-neutral-700 max-md:max-w-full">
-          Your staking:
+          Your staking
         </div>
         <section className="flex gap-5 justify-between mt-2.5 max-md:flex-wrap max-md:max-w-full">
           <div className="text-3xl font-semibold text-neutral-700">
@@ -169,7 +169,7 @@ const StackStepTwoBody: React.FC<StackStepTwoBodyProps> = ({
         </section>
         <hr className="shrink-0 mt-6 h-px border border-solid bg-neutral-300 border-neutral-300 max-md:max-w-full" />
         <div className="mt-6 text-xl font-bold text-neutral-700 max-md:max-w-full">
-          Staking key informations:
+          Staking key informations
         </div>
         <section className="flex gap-5 justify-between mt-6 text-lg font-medium text-neutral-700 max-md:flex-wrap max-md:max-w-full">
           <div className="flex flex-col">
