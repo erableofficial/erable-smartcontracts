488736158f7287e63ef34c476ea2d3d2ff826797
Farouk Allani <81933547+farouk-allani@users.noreply.github.com>
2024-07-09 14:09:15 +0100

    feat: link no farming utility with farming modal

--- components/dashboard/NoFarmingUtilities.tsx
+++ components/dashboard/NoFarmingUtilities.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-import Link from "next/link";
+import LpFarmingModal from "./LpFarmingModal";
 
 const NoFarmingUtilities = () => {
+  const [toggleLpFarmingModal, setToggleLpFarmingModal] =
+    React.useState<boolean>(false);
   return (
     <>
+      <LpFarmingModal
+        setToggleLpFarmingModal={setToggleLpFarmingModal}
+        toggleLpFarmingModal={toggleLpFarmingModal}
+      />
       <div className="flex justify-center items-center px-16  text-lg font-NeueHaas text-neutral-700 max-md:px-5 max-md:max-w-full">
         <div className="flex gap-5 justify-between max-md:flex-wrap">
           <div className="flex gap-2.5 justify-center">
@@ -28,12 +34,12 @@ const NoFarmingUtilities = () => {
       </div>
       <div className="flex justify-center">
         <div className="mt-8 p-6 rounded-3xl border border-solid bg-stone-50 border-stone-300 ">
-          <Link
-            href={"/dashboard/farming"}
-            className="primary-button-sm tracking-wide text-neutral-700 px-6 py-3 text-base font-semibold font-NeueHaas capitalize   "
+          <button
+            className="primary-button-sm tracking-wide text-neutral-700 px-6 py-3 text-base font-semibold font-NeueHaas capitalize"
+            onClick={() => setToggleLpFarmingModal(true)}
           >
             Start LP Farming
-          </Link>
+          </button>
         </div>
       </div>
     </>
