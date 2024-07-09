43ab5535beb9a6889280c147b4ce5cbd4994f63d
Ayoub Amer <ayouub.ameur@gmail.com>
2024-07-09 14:40:09 +0100

    feat: change last faq question text

--- components/ui/Faq.tsx
+++ components/ui/Faq.tsx
@@ -2,113 +2,6 @@ import { Plus, Minus } from "lucide-react";
 import Link from "next/link";
 import * as React from "react";
 
-const faqItems = [
-  {
-    question: "What is erable°?",
-    answer: (
-      <>
-        <div className="font-medium text-lg">
-          <p>
-            erable° is an
-            <span className="font-semibold">impact investment fintech</span>
-            and financial innovation lab dedicated to creating transformative
-            financial products that drive social and environmental change.
-          </p>
-          <p className="mt-7">
-            At the intersection of impact, technology, and collaboration, we
-            believe the challenge isn't finding the funds but redirecting them
-            toward<span className="font-semibold"> impactful projects.</span>{" "}
-            Traditional views on charity and profitability hinder this flow. We
-            advocate for
-            <span className="font-semibold">
-              new financial products and profitability avenues,
-            </span>
-            leveraging technologies like blockchain as key enablers.
-          </p>
-          <p className="mt-7">
-            $ERA token:
-            <br /> We achieve this through our vibrant community, powered by our
-            utility token $ERA, which fuels
-            <span className="font-semibold">
-              our ecosystem and aligns the interests of all stakeholders towards
-              a common goal of impactful change.
-            </span>
-          </p>
-        </div>
-      </>
-    ),
-    link: "#",
-  },
-  {
-    question: "What is the $ERA token?",
-    answer: (
-      <>
-        <div className="font-medium text-lg">
-          <p>
-            $ERA is the utility token at the heart of the erable° business
-            model. Here’s what it brings to the table:
-          </p>
-          <ul style={{ listStyleType: "disc", marginLeft: "20px" }}>
-            <li className="mt-7">
-              <span className="font-semibold">Access Services:</span> Hold $ERA
-              to unlock all our products & services, whether you're an investor,
-              project, or partner.
-            </li>
-            <li className="mt-7">
-              <span className="font-semibold">Earn $ERA:</span> Get rewarded for
-              ecosystem growth through contributions, LP farming, staking, and
-              platform perks.
-            </li>
-            <li className="mt-7">
-              <span className="font-semibold">Govern with $ERA:</span> Have a
-              say in our strategic directions and supported projects, ensuring
-              we stay true to our shared values.
-            </li>
-          </ul>
-          <p className="mt-7">
-            $ERA embodies our commitment to fair value redistribution across
-            erable° and its brands.
-          </p>
-        </div>
-      </>
-    ),
-    link: "#",
-  },
-  {
-    question: "Where to get $ERA?",
-    answer: (
-      <>
-        <div className="font-medium text-lg">
-          <p>
-            You can{" "}
-            <span className="font-semibold">
-              acquire $ERA on Uniswap, a leading decentralized exchange{" "}
-            </span>
-            (DeX) known for its ease of use and robust security features.
-          </p>
-          <p className="mt-7">
-            Steps to Acquire $ERA:
-            <ol style={{ listStyleType: "decimal", marginLeft: "20px" }}>
-              <li>Set Up a Wallet (full list here)</li>
-              <li>Connect to Uniswap</li>
-              <li>Find $ERA</li>
-              <li>Swap $USDC for $ERA</li>
-              <li>View $ERA in Your Wallet</li>
-            </ol>
-          </p>
-        </div>
-      </>
-    ),
-    link: "#",
-  },
-  {
-    question: "How to bridge as a $CLAP holder?",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    link: "#",
-  },
-];
-
 type FAQItemProps = {
   question: string;
   answer: React.ReactNode;
@@ -172,6 +65,153 @@ const FAQItem: React.FC<FAQItemProps> = ({ question, answer, link }) => {
 };
 
 const Faq: React.FC = () => {
+  const faqItems = [
+    {
+      question: "What is erable°?",
+      answer: (
+        <>
+          <div className="font-medium text-lg">
+            <p>
+              erable° is an
+              <span className="font-semibold">impact investment fintech</span>
+              and financial innovation lab dedicated to creating transformative
+              financial products that drive social and environmental change.
+            </p>
+            <p className="mt-7">
+              At the intersection of impact, technology, and collaboration, we
+              believe the challenge isn't finding the funds but redirecting them
+              toward<span className="font-semibold"> impactful projects.</span>{" "}
+              Traditional views on charity and profitability hinder this flow.
+              We advocate for
+              <span className="font-semibold">
+                new financial products and profitability avenues,
+              </span>
+              leveraging technologies like blockchain as key enablers.
+            </p>
+            <p className="mt-7">
+              $ERA token:
+              <br /> We achieve this through our vibrant community, powered by
+              our utility token $ERA, which fuels
+              <span className="font-semibold">
+                our ecosystem and aligns the interests of all stakeholders
+                towards a common goal of impactful change.
+              </span>
+            </p>
+          </div>
+        </>
+      ),
+      link: "#",
+    },
+    {
+      question: "What is the $ERA token?",
+      answer: (
+        <>
+          <div className="font-medium text-lg">
+            <p>
+              $ERA is the utility token at the heart of the erable° business
+              model. Here’s what it brings to the table:
+            </p>
+            <ul style={{ listStyleType: "disc", marginLeft: "20px" }}>
+              <li className="mt-7">
+                <span className="font-semibold">Access Services:</span> Hold
+                $ERA to unlock all our products & services, whether you're an
+                investor, project, or partner.
+              </li>
+              <li className="mt-7">
+                <span className="font-semibold">Earn $ERA:</span> Get rewarded
+                for ecosystem growth through contributions, LP farming, staking,
+                and platform perks.
+              </li>
+              <li className="mt-7">
+                <span className="font-semibold">Govern with $ERA:</span> Have a
+                say in our strategic directions and supported projects, ensuring
+                we stay true to our shared values.
+              </li>
+            </ul>
+            <p className="mt-7">
+              $ERA embodies our commitment to fair value redistribution across
+              erable° and its brands.
+            </p>
+          </div>
+        </>
+      ),
+      link: "#",
+    },
+    {
+      question: "Where to get $ERA?",
+      answer: (
+        <>
+          <div className="font-medium text-lg">
+            <p>
+              The $CLAP token bridge allows holders to bridge their $CLAP tokens
+              from the Cardano blockchain to the Polygon blockchain.
+            </p>
+            <p className="mt-7">
+              The bridge is opened with our partner Chainport, a fully-audited
+              bridging platform specializing in altcoins. The Chainport bridge
+              already supported the bridging of Cardano Native Tokens such as
+              $COPI, $NMKR or $MELD.
+            </p>
+          </div>
+        </>
+      ),
+      link: "#",
+    },
+    {
+      question: "How to bridge as a $CLAP holder?",
+      answer: (
+        <>
+          <div className="font-medium text-lg">
+            <p>
+              The $CLAP token bridge allows holders to bridge their $CLAP tokens
+              from the Cardano blockchain to the Polygon blockchain.
+            </p>
+            <p className="mt-7">
+              The bridge is opened with our partner Chainport, a fully-audited
+              bridging platform specializing in altcoins. The Chainport bridge
+              already supported the bridging of Cardano Native Tokens such as
+              $COPI, $NMKR or $MELD.
+            </p>
+            <p className="mt-7">
+              To{" "}
+              <span className="font-semibold">
+                bridge your $CLAP tokens to Polygon, you must go to the
+                Chainport
+              </span>{" "}
+              application and follow the process. To do this, you'll need your
+              wallets:
+            </p>
+            <ul style={{ listStyleType: "disc", marginLeft: "20px" }}>
+              <li className="mt-7">
+                <span className="font-semibold">Cardano: a Nami wallet</span>
+                (Chainport's only Cardano wallet today) holding your $CLAP
+                tokens.
+              </li>
+              <li>
+                <span className="font-semibold">
+                  Polygon: a compatible EVM wallet{" "}
+                </span>{" "}
+                (full list here)
+              </li>
+            </ul>
+            <p className="mt-7">
+              The whole process <span className="font-semibold">takes at most 4 minutes</span>, including
+              transaction times. You'll need to pay the Cardano transaction fee
+              and the Chainport platform fee of 0.3% of the total $CLAP you wish
+              to bridge.
+            </p>
+            <p className="mt-7">
+              Example: $100,000CLAP to bridge with 0.3% fee $99,700CLAP on
+              Polygon These fees will be more than covered by the incentives
+              offered to early $CLAP holders.
+            </p>
+          </div>
+        </>
+      ),
+      link: "#",
+    },
+  ];
+
   return (
     <section className="self-stretch p-[90px] max-lg:px-5 max-sm:py-10 ">
       <div className="flex gap-5 max-lg:flex-col max-lg:gap-0">
